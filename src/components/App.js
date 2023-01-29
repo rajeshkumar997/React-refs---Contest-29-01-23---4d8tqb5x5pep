f6143d7f76c86acbfd0212c042b06d29a8881b6b
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-29 16:30:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,32 +2,47 @@ import React,{useState,useEffect,useRef} from 'react'
 import '../styles/App.css';
 const App = () => {
 
-//code here 
-  const emailVal = useRef("")
-  const passValue = useRef("")
+
+  const emailInput = useRef(null)
+  const passInput = useRef(null)
 
 const [email, setEmail] = useState("");
 const [pass, setPass] = useState("");
+const [emailVal, setEmailVal] = useState("");
+const [passVal, setPassVal] = useState("");
 
-useEffect(()=>{
-  emailVal.current = email;
-  passValue.current = pass;
-})
+
+
+const handleSubmitBtn = () => {
+  setEmailVal("");
+    setPassVal("");
+  if(email.length === 0){
+    emailInput.current.focus(); 
+  }else if(pass.length === 0){
+    passInput.current.focus();
+  } else{
+    setEmailVal(email);
+    setPassVal(pass);
+    setEmail("");
+  setPass("");
+  }
+  
+}  
 
 
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" value={email}  onChange={(e)=> setEmail(e.target.value)}/><br required/>
+      <input id="inputEmail" type="text" value={email} ref={emailInput}  onChange={(e)=> setEmail(e.target.value)}/><br required/>
       Password
-      <input id="inputPassword" type="text" value={pass}  onChange={(e)=> setPass(e.target.value)}/><br required/>
-      <button id="submitButton" onClick={()=> {}}>Submit</button><br/>
-      <p id="emailText">Your Email : {emailVal.current}</p>
-      <p id ="passwordText">Your Password : {passValue.current}</p>
+      <input id="inputPassword" type="text" value={pass} ref={passInput}  onChange={(e)=> setPass(e.target.value)}/><br required/>
+      <button id="submitButton" onClick={()=> handleSubmitBtn()}>Submit</button><br/>
+      <p id="emailText">Your Email : {emailVal}</p>
+      <p id ="passwordText">Your Password : {passVal}</p>
       
     </div>
   )
 }
 
 
-export default App;
+export default App;
